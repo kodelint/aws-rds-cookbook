3922b8b1ce5f8c16cf2439d722cd387186e87362
Satyajit Roy <kodelint@gmail.com>
2020-04-15 13:43:34 -0700

    Some Changes

--- providers/default.rb
+++ providers/default.rb
@@ -36,6 +36,6 @@ action :delete do
 end
 
 def load_current_resource
-    @current_resource = Chef::Resource::AwsRds.new(new_resource.id)
+    @current_resource = Chef::Resource::Aws::RDS.new(new_resource.id)
     @current_resource.exists = instance.exists?
 end
